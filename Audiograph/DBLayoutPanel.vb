e99bae33c616270da901b35f7fa0cc197bd1796f
Dylan Miller <dmanmiller2004@gmail.com>
2023-05-24 21:21:31 -0400

    Remove old vb files

--- Audiograph/DBLayoutPanel.vb
+++
@@ -1,9 +0,0 @@
-﻿
-Public Class DBLayoutPanel
-    Inherits TableLayoutPanel
-
-    Public Sub New()
-        MyBase.New
-        Me.SetStyle(ControlStyles.UserPaint Or ControlStyles.AllPaintingInWmPaint Or ControlStyles.OptimizedDoubleBuffer, True)
-    End Sub
-End Class
